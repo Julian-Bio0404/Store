fcf62cabbc1d2240db0c18daf83d33233ad1f0c4
Julian-Bio0404 <juliancamilohermida@hotmail.com>
2022-09-26 20:57:46 -0500

    Add query params

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const faker = require('faker');
 const app = express();
 const port = 3000;
 
@@ -7,16 +8,17 @@ app.get('/', (req, res) => {
 });
 
 app.get('/products', (req, res) => {
-  res.json([
-    {
-      name: 'Product 1',
-      price: 1000
-    },
-    {
-      name: 'Product 2',
-      price: 990
-    }
-  ]);
+  const products = [];
+  const { size } = req.query;
+  const limit = size || 10;
+  for (let index = 0; index < limit; index++){
+    products.push({
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price(), 10),
+      image: faker.image.imageUrl(),
+    });
+  }
+  res.json(products);
 });
 
 app.get('/products/:id', (req, res) => {
@@ -27,6 +29,20 @@ app.get('/products/:id', (req, res) => {
     price: 1000
   });
 });
+
+
+app.get('/users', (req, res) => {
+  const { limit, offset } = req.query;
+  if (limit && offset) {
+    res.json({
+      limit,
+      offset
+    });
+  } else {
+    res.send('No hay parametros');
+  }
+});
+
 
 app.listen(port, () => {
   console.log('Servidor corriendo en el puerto ' + port)
