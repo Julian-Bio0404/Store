89a3e0bd0db7822baebe0683b72cfa443da42e11
Julian-Bio0404 <juliancamilohermida@hotmail.com>
2022-09-26 20:34:08 -0500

    Add fake endpoints

--- index.js
+++ index.js
@@ -4,7 +4,29 @@ const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello world!');
-})
+});
+
+app.get('/products', (req, res) => {
+  res.json([
+    {
+      name: 'Product 1',
+      price: 1000
+    },
+    {
+      name: 'Product 2',
+      price: 990
+    }
+  ]);
+});
+
+app.get('/products/:id', (req, res) => {
+  const id = req.params.id;
+  res.json({
+    id,
+    name: 'Product ' + id,
+    price: 1000
+  });
+});
 
 app.listen(port, () => {
   console.log('Servidor corriendo en el puerto ' + port)
