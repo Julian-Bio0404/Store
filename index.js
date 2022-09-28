8299663c399eecf9ce4910e0251a7d36d6b547c2
Julian-Bio0404 <juliancamilohermida@hotmail.com>
2022-09-27 21:50:51 -0500

    Modularize components

--- index.js
+++ index.js
@@ -1,35 +1,12 @@
 const express = require('express');
-const faker = require('faker');
+const routerApi = require('./routes');
+
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello world!');
 });
-
-app.get('/products', (req, res) => {
-  const products = [];
-  const { size } = req.query;
-  const limit = size || 10;
-  for (let index = 0; index < limit; index++){
-    products.push({
-      name: faker.commerce.productName(),
-      price: parseInt(faker.commerce.price(), 10),
-      image: faker.image.imageUrl(),
-    });
-  }
-  res.json(products);
-});
-
-app.get('/products/:id', (req, res) => {
-  const id = req.params.id;
-  res.json({
-    id,
-    name: 'Product ' + id,
-    price: 1000
-  });
-});
-
 
 app.get('/users', (req, res) => {
   const { limit, offset } = req.query;
@@ -43,6 +20,7 @@ app.get('/users', (req, res) => {
   }
 });
 
+routerApi(app);
 
 app.listen(port, () => {
   console.log('Servidor corriendo en el puerto ' + port)
