e2a244f4bd01cde54ba2b74144520f347faf7790
Julian-Bio0404 <juliancamilohermida@hotmail.com>
2022-09-27 22:43:10 -0500

    Create product endpoint

--- index.js
+++ index.js
@@ -4,22 +4,8 @@ const routerApi = require('./routes');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello world!');
-});
-
-app.get('/users', (req, res) => {
-  const { limit, offset } = req.query;
-  if (limit && offset) {
-    res.json({
-      limit,
-      offset
-    });
-  } else {
-    res.send('No hay parametros');
-  }
-});
-
+// Add middleware
+app.use(express.json());
 routerApi(app);
 
 app.listen(port, () => {
